4e6c538335ee0affa188593d524d7c3c38e8cf40
bee60990 <155386705+bee60990@users.noreply.github.com>
2025-02-10 13:29:47 +0100

    Added footer & adapted styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Weather App</h1>;
+  return (
+    <div className="main">
+      <h1>Weather App</h1>
+      <footer>
+        This project has been coded by Julia and is hosted on{" "}
+        <a
+          href="https://github.com/bee60990/weather_project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
